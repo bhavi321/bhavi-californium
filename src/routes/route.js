12a785091e413f3493e6ad2c02cba5d1e753efb5
bhavi321 <bhavialkhaniya5@gmail.com>
2022-12-08 17:44:42 +0530

    code update 8-12-22 5:35pm

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const helperfn= require('../helper-function')
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+
+router.get('/test-you', function(req,res){
+    console.log('my name is',helperfn.greet)
+    helperfn.greet('asli')
+    res.send('we understand')
+});
 module.exports = router;
