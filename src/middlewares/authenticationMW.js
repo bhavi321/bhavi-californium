6a7f64eadf1502a434097bf8493df500c84d3e72
bhavi321 <bhavialkhaniya5@gmail.com>
2022-12-27 14:57:01 +0530

    a

--- src/middlewares/authenticationMW.js
+++ src/middlewares/authenticationMW.js
@@ -7,8 +7,8 @@ const authenticateUser=async function(req,res,next){
     let decodedToken=jwt.verify(token,"Function-up californium secret key")
     if(!decodedToken)
     res.send({msg:"token is invalid"})
-    
-    
+if(decodedToken._id!=req.params.userId)
+    return res.send({status:false,msg:"The loggedin User is not authorized "})
     next()
 }
 module.exports.authenticateUser=authenticateUser
